c20bc52a1aae61635c1a3ef7e8ee909e8c8e35a0
Edoardo Carradori <edoardo.carradori.96@gmail.com>
2023-02-25 19:03:45 +0100

    First commit

---
+++ code/01 - Variabili e Tipi.py
@@ -0,0 +1,16 @@
+myint = 7
+# myfloat = 7.0
+myfloat = float(7)
+mystring = 'hello'
+mystring = "Non preoccuparti dell'apostrofo"
+
+one = 1
+two = 2
+three = one + two
+
+hello = "hello"
+world = "world"
+helloworld = hello + " " + world
+
+# Questo non funzionerà!
+# print one + two + hello
